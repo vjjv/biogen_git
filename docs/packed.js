793efa0eda28abff7f0f5d6ebfe5c56fb59e6354
Bastien Sarolea <sarobastien@gmail.com>
2025-01-17 15:51:59 +0100

    06

--- docs/packed.js
+++ docs/packed.js
@@ -26032,16 +26032,16 @@ console.info(`SDK: ${environment_namespaceObject.l} \
     document.getElementById('canvas').replaceWith(session.output.live);
 
     // const { lenses } = await cameraKit.lensRepository.loadLensGroups(['1c840cc0-bead-4a6d-8328-1fbe4a5ba67a']);
-    // const { lenses } = await cameraKit.lensRepository.loadLensGroups(['bf816633-dd8b-45e8-9603-4a9859acdd52']);
+    const { lenses } = await cameraKit.lensRepository.loadLensGroups(['f7f4e367-f4b3-4de5-8e81-e9c842f2bf0b']);
     // session.applyLens(lenses[3]); 
-    // session.applyLens(lenses[0]); 
+    session.applyLens(lenses[0]); 
 
     //v2 go to https://my-lenses.snapchat.com -> lens scheduler to define group and lens to apply
-    const lens = await cameraKit.lensRepository.loadLens(
-        'f2510933-b9c3-49da-aeda-4c57fe0edd7e', //lens
-        'f7f4e367-f4b3-4de5-8e81-e9c842f2bf0b'  //group 
-      );
-    await session.applyLens(lens)
+    // const lens = await cameraKit.lensRepository.loadLens(
+    //     'f2510933-b9c3-49da-aeda-4c57fe0edd7e', //lens
+    //     'f7f4e367-f4b3-4de5-8e81-e9c842f2bf0b'  //group 
+    //   );
+    // await session.applyLens(lens)
 
 
 
